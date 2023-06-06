8fc2bce37959891b326bd9cb3f94d9cdbdd34410
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-06-06 16:32:18 -0300

    Editar e Atualizar prontos

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -19,7 +19,7 @@ namespace Projeto_Gamer_MVC.Controllers
         {
             _logger = logger;
         }
-    
+
         Context c = new Context(); // Instancia do objeto
 
         // ***METODO LISTAR*** //
@@ -36,14 +36,13 @@ namespace Projeto_Gamer_MVC.Controllers
         public IActionResult Cadastrar(IFormCollection form)
         {
             Jogador novoJogador = new Jogador();
-            Equipe e = new Equipe();
             novoJogador.Nome = form["Nome"].ToString();
             novoJogador.Email = form["Email"].ToString();
             novoJogador.Senha = form["Senha"].ToString();
-            novoJogador.IdEquipe = int.Parse(form["Equipe"]!);
+            novoJogador.IdEquipe = int.Parse(form["IdEquipe"].ToString());
             c.Jogador.Add(novoJogador);
             c.SaveChanges();
-            
+
             return LocalRedirect("~/Jogador/Listar");
 
         }
@@ -64,27 +63,33 @@ namespace Projeto_Gamer_MVC.Controllers
         {
             Jogador j = c.Jogador.First(j => j.IdJogador == id);
             ViewBag.Jogador = j;
+            ViewBag.Equipe = c.Equipe.ToList(); // Guarda a Lista de Equipes
             return View("Edit");
         }
 
         // ***METODO ATUALIZAR*** //
         [Route("Atualizar")]
-        public IActionResult Atualizar(IFormCollection form, Jogador j)
+        public IActionResult Atualizar(IFormCollection form)
         {
             Jogador novoJogador = new Jogador();
-            novoJogador.Nome = j.Nome;
-            novoJogador.Email = j.Email;
-            novoJogador.Senha = j.Senha;
 
-            Jogador jogador = c.Jogador.First(x => x.IdJogador == j.IdJogador);
-            jogador.Nome = novoJogador.Nome;
-            jogador.Email = novoJogador.Email;
-            jogador.Senha = novoJogador.Senha;
+            novoJogador.IdJogador = int.Parse(form["IdJogador"].ToString());
+            novoJogador.Nome = form["Nome"].ToString();
+            novoJogador.Email = form["Email"].ToString();
+            novoJogador.Senha = form["Senha"].ToString();
+            novoJogador.IdEquipe = int.Parse(form["IdEquipe"].ToString());
 
-            c.Jogador.Update(jogador);
+            Jogador jogadorBuscado = c.Jogador.First(j => j.IdJogador == novoJogador.IdJogador);
+
+            jogadorBuscado.Nome = novoJogador.Nome;
+            jogadorBuscado.Email = novoJogador.Email;
+            jogadorBuscado.Senha = novoJogador.Senha;
+            jogadorBuscado.IdEquipe = novoJogador.IdEquipe;
+
+            c.Jogador.Update(jogadorBuscado);
             c.SaveChanges();
 
-            return LocalRedirect("~/Jogador/Atualizar");
+            return LocalRedirect("~/Jogador/Listar");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
